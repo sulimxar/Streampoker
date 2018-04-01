de7da7b9ed720eede4751c3e941294fbce8d34fb
Kreol <vharchenko@gmail.com>
2018-04-01 21:54:59 +0200

    Firebase prod settings

--- Streampoker/src/environments/environment.prod.ts
+++ Streampoker/src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyAxDDUznkEUeeVojIIvFwCBqeh9-Q1-oq4',
+    authDomain: 'streampoker-555.firebaseapp.com',
+    databaseURL: 'https://streampoker-555.firebaseio.com',
+    projectId: 'streampoker-555',
+    storageBucket: 'streampoker-555.appspot.com',
+    messagingSenderId: '554962714878'
+  }
 };
