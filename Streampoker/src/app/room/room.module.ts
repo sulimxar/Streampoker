a210112402e888e6a5f0f07191f7562f781ef6e1
Kreol <vharchenko@gmail.com>
2018-05-21 20:14:24 +0200

    Fixed Home component location

--- Streampoker/src/app/room/room.module.ts
+++ Streampoker/src/app/room/room.module.ts
@@ -2,7 +2,7 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { UserAuthGuardServiceInjectionToken } from '@shared.module';
-import { HomeComponent } from './components/home/home/home.component';
+import { HomeComponent } from './components/home/home.component';
 import { RoomComponent } from './components/room/room.component';
 
 @NgModule({
