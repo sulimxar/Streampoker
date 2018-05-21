5efed04ceca7520ee39574c8757a102426f26b64
Kreol <vharchenko@gmail.com>
2018-05-21 13:24:51 +0200

    scss + 1 version of room home page

--- Streampoker/src/app/room/components/room/room.component.ts
+++ Streampoker/src/app/room/components/room/room.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-room',
   templateUrl: './room.component.html',
-  styleUrls: ['./room.component.css']
+  styleUrls: ['./room.component.scss']
 })
 export class RoomComponent implements OnInit {
 
