74e8098bbcd34115fbb44f396dd8f7545b5f1ff7
Kreol <vharchenko@gmail.com>
2018-10-06 21:22:29 +0200

    Added tooltips for badges

--- Streampoker/src/app/room/components/mark-badge/mark-badge.component.ts
+++ Streampoker/src/app/room/components/mark-badge/mark-badge.component.ts
@@ -1,12 +1,13 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { markDirty } from '@angular/core/src/render3';
+declare var $;
+import { Component, OnInit, Input, AfterViewInit } from '@angular/core';
+//import * as $ from 'jquery';
 
 @Component({
   selector: 'app-mark-badge',
   templateUrl: './mark-badge.component.html',
   styleUrls: ['./mark-badge.component.scss']
 })
-export class MarkBadgeComponent implements OnInit {
+export class MarkBadgeComponent implements OnInit, AfterViewInit {
 
   @Input() name: string;
   @Input() mark: string;
@@ -28,4 +29,63 @@ export class MarkBadgeComponent implements OnInit {
   ngOnInit() {
   }
 
+  ngAfterViewInit(): void {
+
+   // $('[data-toggle="tooltip"]').tooltip('toggleEnabled');
+
+    // $('[data-toggle="tooltip"]').mouseleave(function(e) {
+    //   console.log(e);
+    //   $(e.target).tooltip('hide');
+    // });
+
+    $('[data-toggle="tooltip"]').on({
+      click: function (e) {
+         $(e.currentTarget).tooltip('show');
+      }
+    });
+
+    // document.querySelector('[data-toggle="tooltip"]').addEventListener('mouseenter', function () {
+
+    //  // console.log('mouse enter');
+    // });
+
+    // document.querySelector('[data-toggle="tooltip"]').addEventListener('click', function (e) {
+
+    //   console.log('mouse enter', e);
+    // });
+
+    // document.querySelector('[data-toggle="tooltip"]').addEventListener('click', function (e) {
+
+    //   console.log('mouse enter', e);
+    // });
+
+    //$('[data-toggle="tooltip"]').tooltip();
+    $('[data-toggle="tooltip"]').on('shown.bs.tooltip', function (e) {
+      // console.log('shown: ', $(e.target).tooltip('hide'));
+      //$(e.target).tooltip('hide');
+      setTimeout( function() {
+        $(e.target).tooltip('hide');
+      }, 2000);
+     });
+
+    // $('[data-toggle="tooltip"]').on('hidden.bs.tooltip', function () {
+    //  // console.log('hidden');
+    // });
+
+    //$('[data-toggle="popover"]').popover();
+  //   $(function() {
+  //     const e = $('[data-toggle="tooltip"]');
+  //     console.log(e);
+  //     (e as any).tooltip();
+  // });
+  // $(document).ready(function() {
+  //   ($('[data-toggle="tooltip"]') as any).tooltip();
+  //   });
+//   $(document).ready(function() {
+//     ($('[data-toggle="tooltip"]') as any).tooltip( {
+//       delay: {show: 500, hide: 100},
+//       container: 'body'
+//     });
+// });
+  }
 }
